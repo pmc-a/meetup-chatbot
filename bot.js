57495212f8ea61580cb5295aa8fd6c271d8ba638
Peter McAree <pmcaree@flexera.com>
2018-10-13 15:34:00 +0100

    add initial handler for LUIS response

--- bot.js
+++ bot.js
@@ -1,5 +1,10 @@
-const { ActivityTypes } = require('botbuilder');
+const axios = require('axios');
+const { ActivityTypes, CardFactory } = require('botbuilder');
 const { LuisRecognizer } = require('botbuilder-ai');
+
+// Intents
+const GET_MEEUP_INFO_INTENT = 'GetMeetupInfo';
+const GREETING_INTENT = 'Greeting';
 
 class LuisBot {
     constructor(application, luisPredictionOptions, includeApiResults) {
@@ -12,7 +17,27 @@ class LuisBot {
             const results = await this.luisRecognizer.recognize(turnContext);
             const topIntent = results.luisResult.topScoringIntent;
 
+            // TODO: Reply to user with nice heroCard containing Meetup API response details
+            // E.g. image, name, short description etc.
+            const card = CardFactory.heroCard('HeroCard Title!');
+
+            let reply = { type: ActivityTypes.Message };
+            reply.attachments = [card];
+
+            console.log(results.luisResult);
+
             if (topIntent.intent !== 'None') {
+              
+                switch(topIntent.intent) {
+                    case GET_MEEUP_INFO_INTENT:
+                        const result = await axios.get('https://api.meetup.com/2/groups?key=7b2c7229536f2f392f6f524d7d3d1d1&&sign=true&photo-host=public&group_urlname=belfast-js&page=20');
+                        console.log(result.data.results[0].name);
+                        await turnContext.sendActivity(`Meetup name: ${result.data.results[0].name}`);
+                        await turnContext.sendActivity(reply);
+                    case GREETING_INTENT:
+                        await turnContext.sendActivity('Hey! Ask me something to get started, or ask me for some help');
+                }
+
                 await turnContext.sendActivity(`LUIS Top Scoring Intent: ${ topIntent.intent }, Score: ${ topIntent.score }`);
             } else {
                 await turnContext.sendActivity(`No LUIS intents were found.`);
