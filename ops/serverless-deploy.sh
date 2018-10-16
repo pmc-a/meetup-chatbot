00723d2255302437660665c5df49ecf70272d5a1
Peter McAree <pmcaree08@gmail.com>
2018-10-16 11:25:08 +0100

    Update serverless-deploy.sh

--- ops/serverless-deploy.sh
+++ ops/serverless-deploy.sh
@@ -1,3 +1,6 @@
+# Log current dir
+pwd
+
 # Check npm version
 echo NPM Version:
 npm -v
@@ -14,6 +17,9 @@ serverless -v
 
 # Move back to root dir
 cd ..
+
+# Log current dir
+pwd
 
 # Attempt to deploy via serverless framework
 serverless deploy -v
