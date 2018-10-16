685a2103a83f26e631d67247a26454b4fc98f021
Peter McAree <pmcaree08@gmail.com>
2018-10-16 11:59:53 +0100

    Update serverless-deploy.sh

--- ops/serverless-deploy.sh
+++ ops/serverless-deploy.sh
@@ -17,6 +17,7 @@ echo Microsoft App Password: $MICROSOFT_APP_PASSWORD
 npm install -g serverless
 
 # Echo serverless framework version
+echo Serverless Framework version:
 serverless -v
 
 # Move into root dir
@@ -24,6 +25,9 @@ cd meetup-chatbot-dev-app
 
 # Log current dir
 pwd
+
+# Install dependencies
+npm install
 
 # Attempt to deploy via serverless framework
 serverless deploy -v
