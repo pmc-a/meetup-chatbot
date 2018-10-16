0003530ea779bf365170cd5f13ab6dab19add047
Peter McAree <pmcaree08@gmail.com>
2018-10-16 11:21:26 +0100

    Update serverless-deploy.sh

--- ops/serverless-deploy.sh
+++ ops/serverless-deploy.sh
@@ -1,15 +1,19 @@
 # Check npm version
+echo NPM Version:
 npm -v
 
 # Echo AWS environment vars
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
+echo AWS Access Key ID: $AWS_ACCESS_KEY_ID
+echo AWS Secret Access Key: $AWS_SECRET_ACCESS_KEY
 
 # Install serverless framework
 npm install -g serverless
 
 # Echo serverless framework version
 serverless -v
+
+# Move back to root dir
+cd ..
 
 # Attempt to deploy via serverless framework
 serverless deploy -v
